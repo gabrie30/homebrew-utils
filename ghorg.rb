c1b61d45e5ccbc736ad4b1786e9b50a6879d0963
Jay Gabriels <jgabriels30@gmail.com>
2018-05-26 13:12:43 -0700

    update formula

--- ghorg.rb
+++ ghorg.rb
@@ -1,10 +1,10 @@
 class Ghorg < Formula
   desc "easily clone an entire Github org into one directory "
   homepage "https://github.com/gabrie30/ghorg"
-  url "https://github.com/gabrie30/ghorg/archive/v1.0.0.tar.gz"
-  sha256 "d88a6a622dceb9f2ad90c93ebcc3b7ee14445d08a168c6e0a4986ec615726dc6"
+  url "https://github.com/gabrie30/ghorg/archive/v1.0.1.tar.gz"
+  sha256 "fec897a9c3bbc414aba521e47b6c0a24aa85a2b80f7abba7919b6de3d5935ab2"
 
   def install
-    system "make", "install" 
+    system "make", "homebrew"
   end
 end
