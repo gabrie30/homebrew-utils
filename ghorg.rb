9ddc4067d6084e47852c1047d5208ea1c23acca5
Jay Gabriels <jgabriels30@gmail.com>
2018-05-26 14:47:49 -0700

    update makefile

--- ghorg.rb
+++ ghorg.rb
@@ -1,8 +1,8 @@
 class Ghorg < Formula
   desc "easily clone an entire Github org into one directory "
   homepage "https://github.com/gabrie30/ghorg"
-  url "https://github.com/gabrie30/ghorg/archive/v1.0.2.tar.gz"
-  sha256 "68967136ead81cb6d98caa6ecd1b75ea5b2dea64e6b409759e41b081ff670e97"
+  url "https://github.com/gabrie30/ghorg/archive/v1.0.3.tar.gz"
+  sha256 "758240782bea2cf78860c2f7e036c8ae5cec7e0e3fd2227f382868385e5372f1"
 
   depends_on "go" => :build
 
@@ -24,18 +24,4 @@ class Ghorg < Formula
 
     bin.install "ghorg"
   end
-
-  # def conf
-  #   <<-EOF
-  #   # Add your GitHub token
-  #   # Defaults to using key returned by
-  #   # $ security find-internet-password -s github.com  | grep "acct" | awk -F\" '{ print $4 }'
-  #   GHORG_GITHUB_TOKEN=
-  #   # This is where your org_ghorg directory will be created, use absolute pathing
-  #   # Defaults to $HOME/Desktop/
-  #   GHORG_ABSOLUTE_PATH_TO_CLONE_TO=
-  #   # Branch ghorg resets and leaves checked out, defaults to master
-  #   GHORG_BRANCH=
-  #   EOF
-  # end
 end
