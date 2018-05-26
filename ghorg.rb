111cf7e9709abd25b49161f8174c1f700c9499c2
Jay Gabriels <jgabriels30@gmail.com>
2018-05-26 13:42:29 -0700

    update makefile

--- ghorg.rb
+++ ghorg.rb
@@ -5,7 +5,7 @@ class Ghorg < Formula
   sha256 "68967136ead81cb6d98caa6ecd1b75ea5b2dea64e6b409759e41b081ff670e97"
 
   def install
-    system("touch", "~/.ghorg")
-    system("echo", "GHORG_BRANCH=", ">", "~/.ghorg")
+    `touch ~/.ghorg`
+    `echo GHORG_BRANCH= > ~./ghorg`
   end
 end
