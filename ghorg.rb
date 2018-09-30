64c1cb4c2bd5afe532b700df7129ac6b337894e7
Jay Gabriels <jgabriels30@gmail.com>
2018-09-30 11:28:28 -0700

    Update ghorg.rb

--- ghorg.rb
+++ ghorg.rb
@@ -6,22 +6,35 @@ class Ghorg < Formula
 
   depends_on "go" => :build
 
+#   def install
+#     ENV["GOPATH"] = buildpath
+      
+# #     system "go", "get", "./..."
+#     system "go", "get", "github.com/google/go-github"
+#     system "go", "get", "golang.org/x/oauth2"
+#     system "go", "get", "github.com/joho/godotenv"
+#     system "go", "get", "github.com/mitchellh/go-homedir"
+#     system "go", "get", "github.com/fatih/color"
+#     system "go", "get", "github.com/gabrie30/ghorg/cmd"
+#     system "go", "get", "github.com/gabrie30/ghorg/colorlog"
+#     system "go", "get", "github.com/gabrie30/ghorg/config"
+
+#     system "go", "build", "-o", "ghorg"
+
+#     bin.install "ghorg"
+#   end
+  
   def install
     ENV["GOPATH"] = buildpath
-      
-#     system "go", "get", "./..."
-    system "go", "get", "github.com/google/go-github"
-    system "go", "get", "golang.org/x/oauth2"
-    system "go", "get", "github.com/joho/godotenv"
-    system "go", "get", "github.com/mitchellh/go-homedir"
-    system "go", "get", "github.com/fatih/color"
-    system "go", "get", "github.com/gabrie30/ghorg/cmd"
-    system "go", "get", "github.com/gabrie30/ghorg/colorlog"
-    system "go", "get", "github.com/gabrie30/ghorg/config"
 
-    system "go", "build", "-o", "ghorg"
-
-    bin.install "ghorg"
+    bin_path = "#{buildpath}/src/github.com/gabrie30/ghorg"
+    # Copy all files from their current location (GOPATH root)
+    bin_path.install Dir["*"]
+    cd bin_path do
+      # Install the compiled binary into Homebrew's `bin` - a pre-existing
+      # global variable
+      system "go", "build", "-o", bin/"ghorg", "."
+    end
   end
 
   def post_install
